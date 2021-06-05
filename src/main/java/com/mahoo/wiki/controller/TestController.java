79dd2fa79a579e890061ea20891bbd50c920c2f7
mahoo12138 <mahoo12138@qq.com>
2021-06-05 20:41:06 +0800

    2.8 增加自定义配置项

--- src/main/java/com/mahoo/wiki/controller/TestController.java
+++ src/main/java/com/mahoo/wiki/controller/TestController.java
@@ -1,16 +1,26 @@
 package com.mahoo.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+//    @Value("${test.hello}")
+    @Value("${test.hello:TEST}")    /* 含默认值 */
+    private String testHello;
+
+    @RequestMapping("/test/hello")
+    public String testHello(){
+        return testHello +"world!";
+    }
+
     @RequestMapping("/hello")
     public String hello(){
         return "hello world!";
     }
-    @PostMapping("hello/post")
+    @PostMapping("/hello/post")
     public String helloPost(String name){
         return "Hello" + name;
     }
