a250852fc5596d70a406bc9e22cce94e548fad4a
mahoo12138 <mahoo12138@qq.com>
2021-07-30 11:36:32 +0800

    4.7 组件模块化 Header & Footer 关闭unuse-module 校验

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,83 +1,22 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
-    <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-            mode="inline"
-            v-model:selectedKeys="selectedKeys2"
-            v-model:openKeys="openKeys"
-            :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-      >
-        Content
-      </a-layout-content>
-    </a-layout>
-    <a-layout-footer style="text-align: center">
-      Mahoo Ebook
-    </a-layout-footer>
+    <TheHeader/>
+    <router-view/>
+    <TheFooter/>
   </a-layout>
 </template>
 <script lang="ts">
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
 import { defineComponent, ref } from 'vue';
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from "@/components/TheFooter.vue";
 export default defineComponent({
   components: {
+    TheFooter,
     UserOutlined,
     LaptopOutlined,
     NotificationOutlined,
+    TheHeader,
   },
   setup() {
     return {
@@ -90,16 +29,5 @@ export default defineComponent({
 });
 </script>
 <style>
-.logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
 
-
-.site-layout-background {
-  background: #fff;
-}
 </style>
---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Mahoo Ebook
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
