f49e8be532b58497015d794359b2dab42a0dbfef
mahoo12138 <mahoo12138@qq.com>
2021-07-30 11:24:26 +0800

    4.6 网站首页布局开发

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import ElementPlus from 'element-plus';
+// import 'element-plus/lib/theme-chalk/index.css';
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'
+
+import 'ant-design-vue/dist/antd.css';
+
 
 createApp(App).use(store).use(router).use(Antd).mount('#app')
